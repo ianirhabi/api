fb11f1268ef5d442c384ef8171251fd31d6c16e2
alfatih <35949276+ianirhabi@users.noreply.github.com>
2018-06-26 23:39:48 +0700

    Update main.go

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ import (
 //}
 func init() {
 	orm.RegisterModel(new(model.User))
-	orm.RegisterDataBase("default", "mysql", "root:ian!@#$%^@tcp(18.188.151.231:3306)/retrobarbershop_app?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:ian!@#$%^@tcp(localhost:3306)/retrobarbershop_app?charset=utf8")
 }
 
 func main() {
