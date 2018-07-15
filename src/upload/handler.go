aeb85307eba59464bd2ecfba25b79c8c35bb0af2
programmerjalanan <andrianlatif001@gmail.com>
2018-07-15 05:41:00 -0400

    w22

--- src/upload/handler.go
+++ src/upload/handler.go
@@ -142,5 +142,5 @@ func (h *Handler) createnamePhoto(c echo.Context) (e error) {
 
 func (h *Handler) getphoto(c echo.Context) (e error) {
 	fmt.Println("welcome")
-	return c.File("upload/1531647209901.jpg")
+	return c.File("upload/1531647617022.jpg")
 }
