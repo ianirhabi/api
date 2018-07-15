2788faeb6899e56546f96bb74b34bb7f5870927c
programmerjalanan <andrianlatif001@gmail.com>
2018-07-15 09:35:48 -0400

    w2d

--- src/user/service.go
+++ src/user/service.go
@@ -12,12 +12,10 @@ import (
 func Getuser(id int, uon model.User) (e error) {
 	var c echo.Context
 	o := orm.NewOrm()
-	if err := c.Bind(&uon); err == nil {
-		fmt.Println("masuk kesini ", uon)
-		if d := o.Raw("select * from user where id = ?", id).QueryRow(&uon); d == nil {
-			fmt.Println("usernya adalah ", uon)
-			return c.JSON(http.StatusOK, &uon)
-		}
+	fmt.Println("masuk kesini ", uon)
+	if d := o.Raw("select * from user where id = ?", id).QueryRow(&uon); d == nil {
+		fmt.Println("usernya adalah ", uon)
+		return c.JSON(http.StatusOK, &uon)
 	}
 	return e
 }
